5866b33a47d8dcdbe03ff92e99b5b98a4065afd9
matthes <leuffen@continue.de>
2018-03-06 16:36:36 +0100

    removed-some

--- doc/flavor/ubuntu/installer-debian.sh
+++ doc/flavor/ubuntu/installer-debian.sh
@@ -6,12 +6,31 @@
 # See: https://github.com/c7lab/kickstart/opt/installers
 #
 
+set -e
+set -o pipefail
+trap 'on_error $LINENO' ERR;
+PROGNAME=$(basename $0)
+
+function on_error () {
+    echo "Error: ${PROGNAME} on line $1" 1>&2
+    exit 1
+}
+
+ls -l /kickstart
+exit 1;
+
 apt-get update
 apt-get install -y php-cli curl zip composer vim nano git curl sudo
 
-echo "Cloning kickstart scripts..."
-mkdir /kickstart
-git clone --single-branch --depth 1 "https://github.com/c7lab/kickstart.git" /kickstart
+
+
+if [[ ! -e /kickstart ]]
+then
+    echo "Not local development mode: Cloneing https://github.com/c7lab/kickstart.git into /kickstart"
+    echo "Cloning kickstart scripts..."
+    mkdir /kickstart
+    git clone --single-branch --depth 1 "https://github.com/c7lab/kickstart.git" /kickstart
+fi;
 
 echo "Running kickstart install scripts (container/build.sh)..."
 . /kickstart/container/build.sh
